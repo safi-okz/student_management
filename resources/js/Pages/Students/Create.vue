95065b02cc4e96a9a30353f73a6f94cc84def7fa
safi-okz <safi90655@gmail.com>
2024-09-22 22:38:44 +0500

    create password backend

---
+++ resources/js/Pages/Students/Create.vue
@@ -0,0 +1,152 @@
+<template>
+     <Head title="Create Student" />
+
+<AuthenticatedLayout>
+    <template #header>
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">Create Student</h2>
+    </template>
+    <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+            <div class="lg:grid lg:grid-cols-12 lg:gap-x-5">
+                <div class="space-y-6 sm:px-6 lg:px-0 lg:col-span-12">
+                    <form>
+                        <div class="shadow sm:rounded-md sm:overflow-hidden">
+                            <div class="bg-white py-6 px-4 space-y-6 sm:p-6">
+                                <div>
+                                    <h3
+                                        class="text-lg leading-6 font-medium text-gray-900"
+                                    >
+                                        Student Information
+                                    </h3>
+                                    <p class="mt-1 text-sm text-gray-500">
+                                        Use this form to create a new student.
+                                    </p>
+                                </div>
+
+                                <div class="grid grid-cols-6 gap-6">
+                                    <div class="col-span-6 sm:col-span-3">
+                                        <label
+                                            for="name"
+                                            class="block text-sm font-medium text-gray-700"
+                                            >Name</label
+                                        >
+                                        <input
+                                            v-model="formData.name"
+                                            type="text"
+                                            id="name"
+                                            class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm @error text-red-900 focus:ring-red-500 focus:border-red-500 border-red-300 @enderror"
+                                        />
+                                        <p class="text-red-500">
+                                            This field is required
+                                        </p>
+                                    </div>
+
+                                    <div class="col-span-6 sm:col-span-3">
+                                        <label
+                                            for="email"
+                                            class="block text-sm font-medium text-gray-700"
+                                            >Email Address</label
+                                        >
+                                        <input
+                                            v-model="formData.email"
+                                            type="email"
+                                            id="email"
+                                            autocomplete="email"
+                                            class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                        />
+                                    </div>
+
+                                    <div class="col-span-6 sm:col-span-3">
+                                        <label
+                                            for="class_id"
+                                            class="block text-sm font-medium text-gray-700"
+                                            >Class</label
+                                        >
+                                        <select
+                                            v-model="formData.class_id"
+                                            id="class_id"
+                                            class="mt-1 block w-full bg-white border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                        >
+                                            <option value="">
+                                                Select a Class
+                                            </option>
+                                            <option v-for="item in classes.data" :key="item.id" :value="item.id">{{ item.name }}</option>
+                                        </select>
+                                    </div>
+
+                                    <div class="col-span-6 sm:col-span-3">
+                                        <label
+                                            for="section_id"
+                                            class="block text-sm font-medium text-gray-700"
+                                            >Section</label
+                                        >
+                                        <select
+                                            v-model="formData.section_id"
+                                            id="section_id"
+                                            class="mt-1 block w-full bg-white border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                        >
+                                            <option value="">
+                                                Select a Section
+                                            </option>
+                                            <option v-for="section in sections.data" :key="section.id" :value="section.id">{{ section.name }}</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            <div
+                                class="px-4 py-3 bg-gray-50 text-right sm:px-6"
+                            >
+                                <a
+                                    href="#"
+                                    class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mr-4"
+                                >
+                                    Cancel
+                                </a>
+                                <button
+                                    type="submit"
+                                    class="bg-indigo-600 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                >
+                                    Save
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+</AuthenticatedLayout>
+</template>
+
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head, useForm } from '@inertiajs/vue3';
+import axios from 'axios';
+import { watch, ref } from 'vue';
+
+const props = defineProps({
+    classes: {
+        type: Object,
+    }
+});
+
+const formData = useForm({
+    name: '',
+    email: '',
+    class_id: '',
+    section_id: ''
+});
+
+const sections = ref([]);
+
+watch(() => formData.class_id, (newValue) => {
+        console.log('watch class id ', newValue);
+        getSection(newValue);
+});
+
+const getSection = async (classId) => {
+        const response = await axios.get(`/api/sections?class_id=${classId}`);
+        sections.value = response.data;
+        console.log('section ', response);
+}
+
+console.log("classes dataddd ", props.classes);
+</script>
--- resources/js/Pages/Students/Index.vue
+++ resources/js/Pages/Students/Index.vue
@@ -9,12 +9,13 @@
             </div>
 
             <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-              <a
-                href="#"
+              <Link
+                as="link"
+                :href="route('students.create')"
                 class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
               >
                 Add Student
-              </a>
+              </Link>
             </div>
           </div>
 
@@ -87,7 +88,7 @@
   import MagnifyingGlass from '@/Components/Icons/MignifyingGlass.vue';
   import Pagination from '@/Components/Pagination.vue';
   import { ref, watch } from 'vue';
-  import { usePage } from '@inertiajs/vue3';
+  import { Link, usePage } from '@inertiajs/vue3';
   import { Inertia } from '@inertiajs/inertia';
 
   const props = defineProps({
